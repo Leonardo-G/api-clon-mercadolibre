9c51b95cf6d044cc20494dd0f796efd6b25bc11c
leonardog9823 <guanuco43@gmail.com>
2023-06-15 19:53:25 -0300

    schema and interface opinion

---
+++ src/interfaces/opinion.ts
@@ -0,0 +1,16 @@
+import { Document, Types } from "mongoose";
+
+export interface IOpinion {
+    created: Number;
+    idProduct: Types.ObjectId;
+    idUserOpinion: Types.ObjectId;
+    title: string;
+    comment: string;
+    like: number;
+    down: number;
+    rater: number;
+}
+
+export interface IOpinioDocument extends IOpinion, Document {};
+
+export type IOpinionBody = Omit<IOpinion, 'created' | 'like' | 'down'>;
---
+++ src/models/Opinion.models.ts
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+import { IOpinioDocument } from "../interfaces/opinion";
+
+const OpinionSchema: Schema = new mongoose.Schema({
+    created: {
+        type: Number,
+        default: Date.now
+    },
+    idProduct: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    },
+    idUserOpinion: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    like: {
+        type: Number,
+        default: 0
+    },
+    down: {
+        type: Number,
+        default: 0
+    },
+    rate: {
+        type: Number,
+        required: true
+    }
+})
+
+export default mongoose.model<IOpinioDocument>( "Opinion", OpinionSchema );
