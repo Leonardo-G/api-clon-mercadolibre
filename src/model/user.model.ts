e0747425722b7170afb92257d7360cadda505c8f
leonardog9823 <guanuco43@gmail.com>
2023-07-14 04:04:35 -0300

    user schema nest

---
+++ src/model/user.model.ts
@@ -0,0 +1,67 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+    type: String,
+  })
+  username: string;
+
+  @Prop({
+    type: String,
+    default: null,
+  })
+  lastname: string;
+
+  @Prop({
+    type: String,
+    required: true,
+    unique: true,
+  })
+  email: string;
+
+  @Prop({
+    type: String,
+    required: true,
+  })
+  password: string;
+
+  @Prop({
+    type: String,
+    enum: ['user', 'official-store'],
+    default: 'user',
+  })
+  typeUser: string;
+
+  @Prop({
+    type: String,
+    default:
+      '"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"',
+  })
+  imgUrl: string;
+
+  @Prop({
+    type: Number,
+    default: Date.now(),
+  })
+  created: number;
+
+  @Prop({
+    type: Array,
+    default: [],
+  })
+  categories: string[];
+
+  // Cuando el usuario sea una tienda, colocar tags que representan la tienda.
+  @Prop({
+    type: Array,
+    default: [],
+  })
+  tags: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
